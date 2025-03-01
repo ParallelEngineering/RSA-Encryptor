240ef919708bb4e4f6ac3e7d6fb7f6aa7ef86e44
LordofGhost <134922046+LordofGhost@users.noreply.github.com>
2025-03-01 18:23:51 +0100

    create dummy methods

--- src/key.cpp
+++ src/key.cpp
@@ -2,5 +2,14 @@
 #include "key.h"
 
 
-void key::createRSAKey(int& publicKey, int& privateKey) {
+void createRSAKey() {
+
+}
+
+unsigned long int getPrivateKey(std::string& name) {
+    return 230'948'093'874;
+}
+
+unsigned long int getPublicKey(std::string& name) {
+    return 532'098'453'234;
 }
