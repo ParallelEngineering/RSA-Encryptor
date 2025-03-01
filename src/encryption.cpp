c4d720540b8edab211f0eef9f067de9c3e94289c
Jochen <97750753+Jochengehtab@users.noreply.github.com>
2025-03-01 18:47:25 +0100

    Starting point for encryption

--- src/encryption.cpp
+++ src/encryption.cpp
@@ -1,7 +1,5 @@
 #include "encryption.h"
 
-class encryption
-{
-private:
-public:
-};
+std::int8_t encrypt(std::uint8_t data, unsigned long int e, unsigned long N) {
+    return std::pow(data, e);
+}
---
+++ src/encryption.h
@@ -0,0 +1,9 @@
+#include <iostream>
+#include <cstdint>
+
+class encryption
+{
+private:
+public:
+std::int8_t encrypt(std::uint8_t data, unsigned long int e, unsigned long N);
+};
