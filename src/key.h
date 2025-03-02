1c5453d64183faac7438c9f356ccd67f7e73fdd5
LordofGhost <134922046+LordofGhost@users.noreply.github.com>
2025-03-02 13:03:37 +0100

    implement key exist checking

--- src/key.h
+++ src/key.h
@@ -1,13 +1,25 @@
 #include <iostream>
+#include <filesystem>
 
 #define KEY_FOLDER "rsa-keys"
 
+enum {
+    NONE,
+    PUBLIC,
+    PRIVATE,
+    BOTH
+};
 
 class key {
+private:
+    static std::filesystem::path keysPath();
+    static int keyExists(std::string name);
+    static void generatePublicFromPrivate();
+    static void writeKey(std::string name, unsigned long int data);
 
 public:
-    void createRSAKey();
+    static void createRSAKey();
 
-    std::pair<unsigned long int, unsigned long int> getPrivateKey(std::string& name);
-    std::pair<unsigned long int, unsigned long int> getPublicKey(std::string& name);
+    static std::pair<unsigned long int, unsigned long int> getPrivateKey(std::string& name);
+    static std::pair<unsigned long int, unsigned long int> getPublicKey(std::string& name);
 };
