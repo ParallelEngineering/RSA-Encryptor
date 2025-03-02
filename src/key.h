3fd13db70d9d9a231761f1e64d8742db709a9889
LordofGhost <134922046+LordofGhost@users.noreply.github.com>
2025-03-02 13:44:56 +0100

    key file creation

--- src/key.h
+++ src/key.h
@@ -15,11 +15,12 @@ private:
     static std::filesystem::path keysPath();
     static int keyExists(std::string name);
     static void generatePublicFromPrivate();
-    static void writeKey(std::string name, unsigned long int data);
+    static int writeKey(const std::string &name, unsigned long int data, bool isPublic);
+    static std::string base64_encode(std::string &data);
 
 public:
     static void createRSAKey();
 
-    static std::pair<unsigned long int, unsigned long int> getPrivateKey(std::string& name);
-    static std::pair<unsigned long int, unsigned long int> getPublicKey(std::string& name);
+    static std::pair<unsigned long int, unsigned long int> getPrivateKey(std::string &name);
+    static std::pair<unsigned long int, unsigned long int> getPublicKey(std::string &name);
 };
