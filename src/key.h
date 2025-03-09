bc90619d257c9130cc5c427d254286e033572a8b
LordofGhost <134922046+LordofGhost@users.noreply.github.com>
2025-03-09 15:07:30 +0100

    add base64Encode functionality

--- src/key.h
+++ src/key.h
@@ -1,5 +1,7 @@
 #include <iostream>
 #include <filesystem>
+#include <map>
+#include <vector>
 
 #define KEY_FOLDER "rsa-keys"
 
@@ -10,16 +12,21 @@ enum {
     BOTH
 };
 
+const char base64Chars[] = "ABCDEFGHIJKLMNOPQRSTUVWXYZabcdefghijklmnopqrstuvwxyz0123456789+/";
+
 class key {
 private:
     static std::filesystem::path keysPath();
     static int keyExists(std::string name);
     static void generatePublicFromPrivate();
-    static int writeKey(const std::string &name, unsigned long int data, bool isPublic);
-    static std::string base64_encode(std::string &data);
+    static int writeKey(const std::string &name, std::vector<uint8_t> data, bool isPublic);
+    static std::vector<uint8_t> base64Decode(std::string data);
 
 public:
     static void createRSAKey();
+
+    // TODO
+    static std::string base64Encode(std::vector<uint8_t> data);
 
     static std::pair<unsigned long int, unsigned long int> getPrivateKey(std::string &name);
     static std::pair<unsigned long int, unsigned long int> getPublicKey(std::string &name);
